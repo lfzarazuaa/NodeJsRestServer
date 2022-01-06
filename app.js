fd2d1c405153b061fa30875549535c92bf0eb809
lfzarazuaa <lfzarazuaa1400@outlook.com>
2022-01-06 12:19:20 -0600

    Adding express and environment.

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hola Mundo Mundialero");
+require("dotenv").config(); //Read enviroment variables.
+
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Server running on port:', process.env.PORT)
+} ); //Read the PORT value from enviroment.
