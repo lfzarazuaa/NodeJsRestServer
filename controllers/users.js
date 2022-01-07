15c3ca8e9c991fa863b3d21351f324b70ee14caa
lfzarazuaa <lfzarazuaa1400@outlook.com>
2022-01-06 19:11:19 -0600

    Query parameters on controller.

--- controllers/users.js
+++ controllers/users.js
@@ -1,39 +1,43 @@
 const { response, request } = require("express");
 
-const usersGet = (req, res = response) => {
-    res.json({
-        msg: 'get API - controller'
-    });
-}
+const usersGet = (req = request, res = response) => {
+  const { name, id = 0, apiKey } = req.query; // Destructure the object.
+  res.json({
+    msg: "get API - controller",
+    name,
+    id,
+    apiKey
+  });
+};
 
 const usersPost = (req = request, res = response) => {
-    const body = req.body;
-    const name =  req.body.name
-    const age =  req.body.age
-    res.json({
-      msg: "post API",
-      body,
-      name,
-      age
-    });
-  };
+  const body = req.body;
+  const name = req.body.name;
+  const age = req.body.age;
+  res.json({
+    msg: "post API",
+    body,
+    name,
+    age,
+  });
+};
 
 const usersPut = (req, res = response) => {
-    res.json({
-      msg: "put API",
-    });
-  };
+  res.json({
+    msg: "put API",
+  });
+};
 
 const usersPatch = (req, res = response) => {
-    res.status(400).json({
-      msg: "patch API",
-    });
-  };
+  res.status(400).json({
+    msg: "patch API",
+  });
+};
 
 const usersDelete = (req, res = response) => {
-    res.json({
-      msg: "delete API",
-    });
-  };
+  res.json({
+    msg: "delete API",
+  });
+};
 
-module.exports = {usersGet, usersPost, usersPut, usersPatch, usersDelete}
+module.exports = { usersGet, usersPost, usersPut, usersPatch, usersDelete };
